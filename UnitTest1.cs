5543659c7705bd9914b90cd5a0b55f22a2037193
Robin Munn <rmunn@pobox.com>
2024-02-09 11:20:28 +0700

    Remove download test; async sleep tests are enough
    No need to download a large file: the async sleep tests are enough to
    demonstrate the issue. Also no need for the synchronous sleep test; just
    having two async tests is also enough to demonstrate the issue.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -2,34 +2,19 @@ namespace repro;
 
 public class UnitTest1
 {
-    public string FileId100MB = "0B1MVW1mFO2zmdGhyaUJESWROQkE";
-    public string GoogleDriveUrl(string fileId)
-    {
-        return $"https://drive.usercontent.google.com/download?export=download&confirm=t&id={fileId}";
-    }
-
     [Fact]
-    public void SynchronousSleep5Seconds()
+    public async Task SecondAsyncSleep()
     {
-        Console.WriteLine($"Starting sync sleep test at {DateTime.Now}...");
-        Thread.Sleep(5*1000);
-        Console.WriteLine($"After 5-second delay, time is {DateTime.Now}");
-    }
-
-    [Fact]
-    public async Task Download100MegabyteFileFromGoogle()
-    {
-        Console.WriteLine($"Starting download test at {DateTime.Now}...");
-        var http = new HttpClient();
-        var result = await http.GetAsync(GoogleDriveUrl(FileId100MB));
-        Console.WriteLine($"Download completed at {DateTime.Now} with status {result.StatusCode} and length {result.Content.Headers.ContentLength}");
+        Console.WriteLine($"Starting second async sleep test at {DateTime.Now}...");
+        await Task.Delay(TimeSpan.FromSeconds(3));
+        Console.WriteLine($"After 3-second delay, time is {DateTime.Now}");
     }
 
     [Fact]
     public async Task AsyncSleep5Seconds()
     {
         Console.WriteLine($"Starting async sleep test at {DateTime.Now}...");
-        await Task.Delay(TimeSpan.FromSeconds(5));
-        Console.WriteLine($"After 5-second delay, time is {DateTime.Now}");
+        await Task.Delay(TimeSpan.FromSeconds(7));
+        Console.WriteLine($"After 7-second delay, time is {DateTime.Now}");
     }
 }
